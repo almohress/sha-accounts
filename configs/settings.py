5b70f3fe1e94381b4338310659fb2638f665aba2
alimohress <alimohress@gmail.com>
2021-08-30 14:41:15 +0430

    override auth user model

--- configs/settings.py
+++ configs/settings.py
@@ -105,7 +105,9 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
+AUTH_USER_MODEL = 'sha_accounts.User'
+
 SHA_ACCOUNTS = {
     'DEFAULT_ACTIVATION': True,
-    'AUTH_USER_MODEL':'User'
+    'AUTH_USER_MODEL': 'User'
 }
