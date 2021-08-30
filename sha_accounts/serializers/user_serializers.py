d12779eb11e161c96a76e273d0824be5c4c2a689
alimohress <alimohress@gmail.com>
2021-08-30 19:52:14 +0430

    add signin serializers

--- sha_accounts/serializers/user_serializers.py
+++ sha_accounts/serializers/user_serializers.py
@@ -1,4 +1,6 @@
+from django.core.exceptions import ObjectDoesNotExist
 from django.contrib.auth.hashers import make_password
+from djrest_wrapper.exceptions.apis.exceptions import DoesNotExistsExp
 from rest_framework import serializers
 from drf_writable_nested.serializers import WritableNestedModelSerializer
 from ..models.user_models import User
@@ -29,3 +31,28 @@ class UserSignUpResponse(WritableNestedModelSerializer):
     class Meta:
         model = User
         fields = ['username', 'email', 'is_active', 'profile']
+
+
+class UserSignInRequest(serializers.ModelSerializer):
+    username = serializers.CharField(required=True)
+    password = serializers.CharField(write_only=True, required=True)
+
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def login(self):
+        username = self.validated_data.get('username')
+        password = self.validated_data.get('password')
+        try:
+            user = self.Meta.model.objects.get(username=username)
+            if user.check_password(password):
+                pass
+            else:
+                raise DoesNotExistsExp(f'Invalid Credentials')
+        except ObjectDoesNotExist as e:
+            raise DoesNotExistsExp(f'Invalid Credentials')
+
+class UserSignInResponse(UserSignUpResponse):
+    class Meta(UserSignUpResponse.Meta):
+        pass
