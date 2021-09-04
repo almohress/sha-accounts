64e16992bb618fda5857b4434c17788aea9ab63f
alimohress <alimohress@gmail.com>
2021-09-05 01:29:05 +0430

    change settings
    remove SHA_ACCOUNTS.AUTH_USER_MODEL
    format code style

--- sha_accounts/signals/__init__.py
+++ sha_accounts/signals/__init__.py
@@ -1,5 +1,5 @@
 from django.conf import settings
 from .signals import user_logged_in, user_logged_out, user_login_failed
 
-if settings.SHA_ACCOUNTS.get('AUTH_USER_MODEL') == 'User':
+if settings.AUTH_USER_MODEL == 'sha_accounts.User':
     from .receivers import create_profile
